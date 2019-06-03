387fd2b778dcd4bb2ace8c5cb38bff62939b08a6
Tatsuro <contacts@tatsuro.dev>
2019-06-03 20:57:49 +0900

    firebaseセットアップ(74)

---
+++ src/firebase/init.js
@@ -0,0 +1,18 @@
+// firebase, firestoreを使えるようにインポート
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgTXV9oIfyttWmnNf7q6bubW_61ev-2q8",
+  authDomain: "udemy-geo-ninjas-3658b.firebaseapp.com",
+  databaseURL: "https://udemy-geo-ninjas-3658b.firebaseio.com",
+  projectId: "udemy-geo-ninjas-3658b",
+  storageBucket: "udemy-geo-ninjas-3658b.appspot.com",
+  messagingSenderId: "993176629836",
+  appId: "1:993176629836:web:1595bffb16f4c7b3"
+};
+
+// 上記設定でfirebase開始
+const firebaseApp = firebaseConfig.initalizeApp(firebaseConfig);
+// firestoreをエクスポート
+export default firebaseApp.firestore();
