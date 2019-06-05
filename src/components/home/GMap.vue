5a9f86ee5ffd35999398391068f16c70827965d8
Tatsuro <contacts@tatsuro.dev>
2019-06-05 14:33:32 +0900

    ユーザーの位置にマーカーをつける(92)

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -27,6 +27,28 @@ export default {
         minZoom: 3,
         streetViewControl: false
       });
+      // ユーザーのgeolocationを取得し位置にマーカーをつける
+      db.collection("users")
+        .get()
+        .then(users => {
+          users.docs.forEach(doc => {
+            let data = doc.data();
+            if (data.geolocation) {
+              let marker = new google.maps.Marker({
+                position: {
+                  lat: data.geolocation.lat,
+                  lng: data.geolocation.lng
+                },
+                // map: map
+                map
+              });
+              // マーカーにクリックイベント
+              marker.addListener("click", () => {
+                console.log(doc.id);
+              });
+            }
+          });
+        });
     }
   },
   mounted() {
