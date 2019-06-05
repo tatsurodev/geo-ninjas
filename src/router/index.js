8722753bdfeb40122ee5b729eecd2a7b9daa8fb3
Tatsuro <contacts@tatsuro.dev>
2019-06-05 11:51:06 +0900

    GMapコンポーネントにナビゲーションガードを追加(90)
    グローバルにガードが必要かメタデータをチェックして追加する

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,18 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [{
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -24,3 +28,28 @@ export default new Router({
     }
   ]
 })
+
+// グローバルにルーターガード
+router.beforeEach((to, from, next) => {
+  // ルーターが認証が必要かどうかをチェック、requiresAuthがtrue or false
+  // 認証が必要
+  // someメソッドは配列が１つでも条件を満たせばtrueを返す
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // 現ユーザー取得
+    let user = firebase.auth().currentUser;
+    if (user) {
+      // ログインユーザー
+      next()
+    } else {
+      // ログインユーザーでない
+      next({
+        name: 'Login'
+      })
+    }
+    // 認証不要
+  } else {
+    next()
+  }
+})
+
+export default router
